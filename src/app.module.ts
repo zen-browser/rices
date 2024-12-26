ff50da2afd879288db921ca6a2086810f3b90e3b
mr. M <mauro.balades@tutanota.com>
2024-12-26 01:06:47 +0100

    Add throttling configurations for short, medium, and long requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,8 +13,19 @@ import { ThrottlerExceptionFilter } from './common/filters/throttler-exception.f
     }),
     ThrottlerModule.forRoot([
       {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
         ttl: 60000,
-        limit: 10,
+        limit: 100,
       },
     ]),
     GitHubModule,
